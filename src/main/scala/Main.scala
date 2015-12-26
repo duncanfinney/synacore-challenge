d27e36b4669793743360cd813564527e4c5c5e1d
Duncan Finney <duncan.finney@gmail.com>
2015-12-26 07:02:32 -0500

    Adding code to read the binary file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+import java.io.{FileInputStream, FileOutputStream, IOException, File}
+
 object Main extends App {
-  println("hello there")
+  var in: Option[FileInputStream] = None
+  try {
+    in = Some(new FileInputStream("src/main/resources/challenge.bin"))
+    var c = 0
+    while ({c = in.get.read; c != -1}) {
+      println(c)
+    }
+  } catch {
+    case e: IOException => e.printStackTrace
+  } finally {
+    if (in.isDefined) in.get.close
+  }
 }
